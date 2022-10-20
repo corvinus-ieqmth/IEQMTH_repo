3c836faa4c240434ada4ac1037085f4dc5916305
Tothadam80 <toth.adam80@gmail.com>
2022-10-20 20:52:20 +0200

    task 3 done

--- Webservice_IEQMTH/Webservice/Form1.cs
+++ Webservice_IEQMTH/Webservice/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webservice.MNBServiceReference;
 
 namespace Webservice
 {
     public partial class Form1 : Form
     {
+        MNBArfolyamServiceSoapClient MNBArfolyamService = new MNBArfolyamServiceSoapClient();
+
         public Form1()
         {
             InitializeComponent();
+
+            GetXCRates();
+        }
+
+        private void GetXCRates()
+        {
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody() { currencyNames = "EUR", startDate = "2020-01-01", endDate = "2020-06-30" };
+            var response = MNBArfolyamService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
