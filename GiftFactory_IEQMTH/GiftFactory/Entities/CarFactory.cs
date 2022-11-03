4205551ce0e66c0bce8f4f5608ffab183c4477eb
Tothadam80 <toth.adam80@gmail.com>
2022-11-03 23:28:52 +0100

    abstraction done

--- GiftFactory_IEQMTH/GiftFactory/Entities/CarFactory.cs
+++ GiftFactory_IEQMTH/GiftFactory/Entities/CarFactory.cs
@@ -7,13 +7,11 @@ using System.Threading.Tasks;
 
 namespace GiftFactory.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
-       
-
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
