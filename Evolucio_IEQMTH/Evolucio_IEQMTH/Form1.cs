e8c824f384a88d76c81a8974be90157601314ebd
Tothadam80 <toth.adam80@gmail.com>
2022-12-09 08:50:51 +0100

    GameArea created, first test done, label created

--- Evolucio_IEQMTH/Evolucio_IEQMTH/Form1.cs
+++ Evolucio_IEQMTH/Evolucio_IEQMTH/Form1.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace Evolucio_IEQMTH
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+
+            }
+
+            gc.GameOver += Gc_GameOver;
+            gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
         }
     }
 }
